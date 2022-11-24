e1d68698c1688d8c4ff81ef6c8e32da609ccdacc
Matija Novosel <mnovosel5@gmail.com>
2022-11-24 22:53:08 +0100

    Page flipping

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Flipbook class="flipbook" :pages="pages" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Flipbook from "flipbook-vue";
+const pages = ["", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "5.jpg"];
+</script>
+
+<style>
+.flipbook .viewport {
+  width: 90vw;
+  height: 90vh;
+}
+
+.flipbook .bounding-box {
+  box-shadow: 0 0 20px #000;
+}
+</style>
