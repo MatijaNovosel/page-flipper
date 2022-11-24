4097955e44653af47b8091b87a2116a9e9d277cf
Matija Novosel <mnovosel5@gmail.com>
2022-11-24 23:15:11 +0100

    Fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts" setup>
 import Flipbook from "flipbook-vue";
-const pages = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "5.jpg"];
+const pages = ["", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "5.jpg"];
 </script>
 
 <style>
